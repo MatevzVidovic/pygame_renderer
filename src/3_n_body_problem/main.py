8b61e5034c2670feb7431252b7165ecf550f55f6
Matevž Vidovič <matevz.vidovic@flycom.si>
2026-07-11 12:40:59 +0200

    N-body using unit coordinates

--- src/3_n_body_problem/main.py
+++ src/3_n_body_problem/main.py
@@ -4,46 +4,47 @@ import numpy as np
 
 from core.renderer import Elipse, ObjectTree, Rect, Splat, VisualProgram, main
 
-SCREEN_SIZE = (800, 600)
-BALL_RADIUS = 4
-NUM_OF_BALLS = 5
+
+SCREEN_SIZE = 800
+BALL_RADIUS = 0.03
+# NUM_OF_BALLS = 5
 
 
 class NBodyProblem(VisualProgram):
     def __init__(self) -> None:
-        self.width, self.height = SCREEN_SIZE
         self.positions = np.array(
             [
-                [300.0, 400.0],
-                [250.0, 360.0],
+                [0.5, 0.7],
+                [0.7, 0.5],
             ]
         )
         self.velocities = np.array(
             [
-                [2.5, 3.0],
-                [1.7, -2.3],
+                [0.03, 0.02],
+                [0.02, 0.01],
             ]
         )
 
-        self.ball = Elipse(2 * BALL_RADIUS, (79, 209, 197))
+        self.ball = Elipse(2 * BALL_RADIUS * SCREEN_SIZE, (79, 209, 197))
 
     def step(self) -> None:
         self.positions += self.velocities
 
         for point, velocity in zip(self.positions, self.velocities):
-            if point[0] - BALL_RADIUS < 0 or point[0] + BALL_RADIUS > self.height:
+            if point[0] - BALL_RADIUS < 0 or point[0] + BALL_RADIUS > 1:
                 velocity[0] *= -1
-            if point[1] - BALL_RADIUS < 0 or point[1] + BALL_RADIUS > self.width:
+            if point[1] - BALL_RADIUS < 0 or point[1] + BALL_RADIUS > 1:
                 velocity[1] *= -1
 
     def get_object_tree(self) -> ObjectTree:
-        self.positions
-        return [[Splat(self.ball, self.positions[i]) for i in range(np.size(self.positions, 0))]]
+        screen_positions = self.positions * SCREEN_SIZE
+        print(screen_positions)
+        return [[Splat(self.ball, screen_positions[i]) for i in range(np.size(screen_positions, 0))]]
 
 
 def run() -> None:
     visual: VisualProgram = NBodyProblem()
-    main(visual, size=SCREEN_SIZE, title="N-Body Problem")
+    main(visual, size=(SCREEN_SIZE, SCREEN_SIZE), title="N-Body Problem")
 
 
 if __name__ == "__main__":
